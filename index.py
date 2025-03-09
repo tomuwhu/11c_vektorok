51c0a0496c6a3ab59def9ba84e40f0ef03b99146
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-09 19:12:00 +0100

    .

--- index.py
+++ index.py
@@ -2,14 +2,15 @@ from browser import document as D, html as H
 from random import randrange as R
 psz = 0
 vsz = 0
+mpsz = 45
 def f(e):
-  global psz, F, HSZ, vsz
+  global psz, F, HSZ, vsz, mpsz
   psz += 1
-  if psz > 35:
+  if psz > mpsz:
     F.clear()
     F <= H.SPAN("Túl sok próbálkozás!", Class="psz")
   HSZ.clear()
-  HSZ <= 35 - psz
+  HSZ <= mpsz - psz
   af = e.target.id
   if e.target.value == '': return
   v = int(e.target.value)
@@ -86,7 +87,7 @@ if d == 1:
   _d = ''
 else: _d = str(d)
 _c = str(c)
-HSZ = H.SPAN(35, Class="psz")
+HSZ = H.SPAN(mpsz, Class="psz")
 D <= H.H1("Vektorok - 11c") 
 C = H.DIV(Class="f f1")
 C <= H.DIV(f"Koordinátáikkal adottak ") <= [H.SPAN("$$\\vec{a}("+_a1+","+_a2+")$$"), " és ", H.SPAN("$$\\vec{b}("+_b1+","+_b2+")$$"), " helyvektorok."]
